49b7d2dfeb3b002811d79a4c55d3bc7dbc752826
Hongjin Quan <hq48@cornell.edu>
2024-02-28 15:14:32 -0500

    feat: setup docker-compose and account service

---
+++ README.md
@@ -0,0 +1,8 @@
+# Memrizr Application
+Golang, Docker, Traefik, Postgres, Redis, and React/Vue
+
+## Day1: 
+### Setup Docker
+### handlers, routers, and services
+
+![architechure](/architechure.png)
---
+++ account/handler/me.go
@@ -0,0 +1,9 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+// Me handler calls services for getting
+// a user's details
+func (h *Handler) Me(c *gin.Context) {
+	// A *model.User will eventually be added to context in middleware
+}
